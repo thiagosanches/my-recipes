1ab6669a549fb217f1d735c0ad8d5daa9830e290
Thiago Sanches <tsigwt@gmail.com>
2018-01-27 09:42:11 -0200

    Adding global configuration to git

--- init.sh
+++ init.sh
@@ -6,13 +6,17 @@ TOOLS_FOLDER="tools"
 GENERAL_FOLDER="general"
 
 echo "Creating folders..."
-mkdir "$BASE_FOLDER/$REPOSITORIES_FOLDER" 2> /dev/null
-mkdir "$BASE_FOLDER/$TOOLS_FOLDER" 2> /dev/null
-mkdir "$BASE_FOLDER/$GENERAL_FOLDER" 2> /dev/null
+mkdir "$BASE_FOLDER/$REPOSITORIES_FOLDER" 2>/dev/null
+mkdir "$BASE_FOLDER/$TOOLS_FOLDER" 2>/dev/null
+mkdir "$BASE_FOLDER/$GENERAL_FOLDER" 2>/dev/null
+
+echo "Setting up my GIT config..."
+git config --global user.name "Thiago Sanches"
+git config --global user.email tsigwt@gmail.com
 
 echo "Cloning my repositories..."
 cd "$BASE_FOLDER/$REPOSITORIES_FOLDER"
-git clone https://github.com/thiagosanches/my-recipes.git 2> /dev/null
-git clone https://github.com/thiagosanches/trump-engine 2> /dev/null
-git clone https://github.com/thiagosanches/trump-front-end 2> /dev/null
-git clone https://github.com/thiagosanches/arisco 2> /dev/null
+git clone https://github.com/thiagosanches/my-recipes.git 2>/dev/null
+git clone https://github.com/thiagosanches/trump-engine 2>/dev/null
+git clone https://github.com/thiagosanches/trump-front-end 2>/dev/null
+git clone https://github.com/thiagosanches/arisco 2>/dev/null
