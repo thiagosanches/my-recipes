88bdd19b574bbabd5fbb0ba292e4c9a8fef4347e
unknown <tsigwt@gmail.com>
2018-01-26 23:11:25 -0200

    Handling standard errors

--- init.sh
+++ init.sh
@@ -6,13 +6,13 @@ TOOLS_FOLDER="tools"
 GENERAL_FOLDER="general"
 
 echo "Creating folders..."
-mkdir "$BASE_FOLDER/$REPOSITORIES_FOLDER"
-mkdir "$BASE_FOLDER/$TOOLS_FOLDER"
-mkdir "$BASE_FOLDER/$GENERAL_FOLDER"
+mkdir "$BASE_FOLDER/$REPOSITORIES_FOLDER" 2> /dev/null
+mkdir "$BASE_FOLDER/$TOOLS_FOLDER" 2> /dev/null
+mkdir "$BASE_FOLDER/$GENERAL_FOLDER" 2> /dev/null
 
 echo "Cloning my repositories..."
 cd "$BASE_FOLDER/$REPOSITORIES_FOLDER"
-git clone https://github.com/thiagosanches/my-recipes.git
-git clone https://github.com/thiagosanches/trump-engine
-git clone https://github.com/thiagosanches/trump-front-end
-git clone https://github.com/thiagosanches/arisco
+git clone https://github.com/thiagosanches/my-recipes.git 2> /dev/null
+git clone https://github.com/thiagosanches/trump-engine 2> /dev/null
+git clone https://github.com/thiagosanches/trump-front-end 2> /dev/null
+git clone https://github.com/thiagosanches/arisco 2> /dev/null
