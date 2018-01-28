e5d9bef22051c1c5c1f9f11756b038791abd4f29
Thiago Sanches <tsigwt@gmail.com>
2018-01-28 17:40:17 -0200

    Reusing functions and adding scripts to the its folder

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-showMessage(){
-  # Adding some color to the script :)
-  ESC_SEQ="\x1b["
-  RESET=$ESC_SEQ"39;49;00m"
-  A=$ESC_SEQ"32;01m"
-  echo -e "$A $1 $RESET"
-}
+TEMP_FOLDER=/tmp/my-recipes
+
+mkdir $TEMP_FOLDER
+cd $TEMP_FOLDER
+
+# First clone my-recipes to reuse my bash functions
+git clone https://github.com/thiagosanches/my-recipes.git 2>/dev/null
+
+# Reusing my functions
+[ -f $TEMP_FOLDER/utils/utils.sh ] && . $TEMP_FOLDER/utils/utils.sh
 
 BASE_FOLDER="/mnt/c/Users/Thiago"
 REPOSITORIES_FOLDER="repositories"
@@ -22,26 +25,14 @@ showMessage "Setting up my GIT config..."
 git config --global user.name "Thiago Sanches"
 git config --global user.email tsigwt@gmail.com
 
-showMessage "Cloning my repositories..."
 cd "$BASE_FOLDER/$REPOSITORIES_FOLDER"
+showMessage "Cloning my repositories..."
 git clone https://github.com/thiagosanches/my-recipes.git 2>/dev/null
 git clone https://github.com/thiagosanches/trump-engine 2>/dev/null
 git clone https://github.com/thiagosanches/trump-front-end 2>/dev/null
 git clone https://github.com/thiagosanches/arisco 2>/dev/null
 
 showMessage "Installing docker client..."
-sudo apt-get update
-sudo apt-get remove --yes docker docker-engine docker.io
-sudo apt-get install --yes \
-   apt-transport-https \
-   ca-certificates \
-   curl \
-   software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get --yes install docker-ce
+[ -f scripts/install-docker-ce-ubuntu.sh ] && . scripts/install-docker-ce-ubuntu.sh
+
+rm -rf $TEMP_FOLDER 2>/dev/null
---
+++ utils/utils.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+showMessage(){
+  # Adding some color to the script :)
+  ESC_SEQ="\x1b["
+  RESET=$ESC_SEQ"39;49;00m"
+  A=$ESC_SEQ"32;01m"
+  echo -e "$A $1 $RESET"
+}
