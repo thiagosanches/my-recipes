9f72225260e567d43ccfdc218b0713b71970d964
Thiago <tsigwt@gmail.com>
2019-07-19 22:01:52 -0300

    Update the variable

--- linux/utils/utils.sh
+++ linux/utils/utils.sh
@@ -3,7 +3,7 @@
 showMessage(){
   # Adding some color to the script :)
   ESC_SEQ="\x1b["
-  RESET=$ESC_SEQ"39;49;00m"
-  A=$ESC_SEQ"32;01m"
-  echo -e "$A$1$RESET"
+  RESET=${ESC_SEQ}"39;49;00m"
+  A=${ESC_SEQ}"32;01m"
+  echo -e "${A}${1}${RESET}"
 }
