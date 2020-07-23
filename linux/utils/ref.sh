c992d3157ddc43dcfe4409918f3a98ab35682728
Thiago <tsigwt@gmail.com>
2020-07-23 19:26:21 -0300

    Update ref.sh

--- linux/utils/ref.sh
+++ linux/utils/ref.sh
@@ -8,9 +8,9 @@ function ref() {
     if [ -z "$search_parameter" ]; then
         code $REF_FILE
     else
-	grep -i "$search_parameter" $REF_FILE
-        OUTPUT=`grep -i "$search_parameter" $REF_FILE | sed 's/==/\t/g' | awk '{ print $2 }'`
-	echo $OUTPUT | xclip
-	echo $OUTPUT
+		grep -i "$search_parameter" $REF_FILE
+	OUTPUT=`grep -i "$search_parameter" $REF_FILE | sed 's/==/\t/g' | awk '{ print $2 }'`
+		echo $OUTPUT | xclip
+		echo $OUTPUT
     fi
 }
