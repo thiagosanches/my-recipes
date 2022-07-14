46ad43c96d02e76883ca5beb0730187bf3f99345
Thiago Sanches <thiagom@ciandt.com>
2022-07-14 17:39:55 -0300

    Fix some lint issues

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ BASE_FOLDER="$1"
 TEMP_FOLDER="$BASE_FOLDER/tmp"
 
 # First clone my-recipes to reuse my bash functions.
-git clone https://github.com/thiagosanches/my-recipes.git $TEMP_FOLDER
+git clone https://github.com/thiagosanches/my-recipes.git "$TEMP_FOLDER"
 
 # Re-using my functions.
-[ -f $TEMP_FOLDER/linux/utils/utils.sh ] && . $TEMP_FOLDER/linux/utils/utils.sh
+[ -f "$TEMP_FOLDER"/linux/utils/utils.sh ] && . "$TEMP_FOLDER"/linux/utils/utils.sh
 
 REPOSITORIES_FOLDER="r"
 TOOLS_FOLDER="t"
@@ -22,16 +22,16 @@ showMessage "Setting up my GIT config..."
 git config --global user.name "Thiago Sanches"
 git config --global user.email tsigwt@gmail.com
 
-cd "$BASE_FOLDER/$REPOSITORIES_FOLDER"
+cd "$BASE_FOLDER/$REPOSITORIES_FOLDER" || exit
 showMessage "Cloning my repositories..."
 git clone https://github.com/thiagosanches/my-recipes.git
 
 # Running my scripts.
-cd $BASE_FOLDER/$REPOSITORIES_FOLDER/my-recipes
+cd "$BASE_FOLDER"/"$REPOSITORIES_FOLDER"/my-recipes
 
-showMessage "Installing docker client..."
-[ -f linux/scripts/install-docker-ce-ubuntu.sh ] && . linux/scripts/install-docker-ce-ubuntu.sh
-[ -f linux/scripts/install-i3.sh ] && . linux/scripts/install-i3.sh
+showMessage "Running some scripts..."
+[ -f linux/scripts/install-docker-ce-ubuntu.sh ] && . linux/bin/install-docker-ce-ubuntu.sh
+[ -f linux/scripts/install-i3.sh ] && . linux/bin/install-i3.sh
 
 
-rm -rf $TEMP_FOLDER 2>/dev/null
+rm -rf "$TEMP_FOLDER" 2>/dev/null
