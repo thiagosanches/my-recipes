218fc72873bc4f9caaaa7737b43741d48d1ab50d
Thiago Sanches <thiagom@ciandt.com>
2022-07-14 17:25:32 -0300

    Update

--- linux/bin/install-i3.sh
+++ linux/bin/install-i3.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install xss-lock dex i3 gnome-terminal rofi feh lxappearance gnome-tweaks adwaita-icon-theme-full picom network-manager-gnome compton -y
+sudo apt-get install -y xss-lock \
+dex \
+i3 \
+gnome-terminal \
+rofi \
+feh \
+lxappearance \
+gnome-tweaks \
+adwaita-icon-theme-full \
+network-manager-gnome \
+compton 
