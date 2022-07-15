67362678c5f8f65441ca2c27f502b9a4f2ed6bf6
Thiago Sanches <tsigwt@gmail.com>
2022-07-15 17:26:08 -0300

    Fix aws-profile-setter.sh with shellcheck recommendations

--- linux/bin/aws-profile-setter.sh
+++ linux/bin/aws-profile-setter.sh
@@ -2,26 +2,26 @@
 
 if [ ! -f ~/.aws/credentials ]
 then
-    echo "~/.aws/credentials file does not exists"
+    echo "$HOME/.aws/credentials file does not exists!"
     exit 1
 fi
 
 COUNT=0
-cat ~/.aws/credentials | grep -E "\[.*\]" | sed -e 's/\]//g' | sed 's/\[//g' | sort | uniq > all-aws-profile.txt
+grep -E "\[.*\]" ~/.aws/credentials | sed -e 's/\]//g' | sed 's/\[//g' | sort | uniq > all-aws-profile.txt
 mapfile -t ARRAY < all-aws-profile.txt
 
 echo "" > out.txt
-for i in $(cat all-aws-profile.txt)
+grep -v '^ *#' < all-aws-profile.txt | while IFS= read -r line
 do
     COUNT=$(echo $COUNT + 1 | bc)
-    echo "$COUNT \"$i\" off" >> out.txt
+    echo "$COUNT \"$line\" off" >> out.txt
 done
 
 
-OPTIONS=$(cat out.txt | tr "\n" " ")
+OPTIONS=$(tr "\n" " " < out.txt)
 
-INDEX=$(echo $OPTIONS | xargs dialog --radiolist "Select the AWS profile" 0 50 0 3>&1 1>&2 2>&3 3>&-)
-INDEX=$(echo $INDEX - 1 | bc)
+INDEX=$(echo "$OPTIONS" | xargs dialog --radiolist "Select the AWS profile" 0 50 0 3>&1 1>&2 2>&3 3>&-)
+INDEX=$(echo "$INDEX" - 1 | bc)
 export AWS_PROFILE=${ARRAY[INDEX]}
 clear
 
