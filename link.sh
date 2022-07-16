ec11c20864d87959579cc52dc9fddc836cf7ff69
Thiago Sanches <tsigwt@gmail.com>
2022-07-16 08:30:21 -0300

    Add shellcheck instruction to avoid that verification

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@
 rm -rf ln.sh dir.sh
 
 # creates the folder structure if needed (mkdir -p).
+# I want this to output $HOME without expansion cause bash it will execute it anyway, resolving the variable.
+# shellcheck disable=SC2016
 find linux/ -type f -not -path "*bin*" -exec dirname {} \; | sed 's#linux#$HOME#1' | sed 's#$HOME#mkdir -p $HOME#1' >> dir.sh && bash dir.sh
 
 # looks for everything under the linux folder but the bin folder.
