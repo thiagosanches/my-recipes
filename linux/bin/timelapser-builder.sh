2def707b735bdfa979b32a1741404db2d416862d
Thiago Sanches <tsigwt@gmail.com>
2022-07-16 10:56:44 -0300

    Update timelapser-builder.sh

--- linux/bin/timelapser-builder.sh
+++ linux/bin/timelapser-builder.sh
@@ -4,7 +4,7 @@ while true
 do
     FILENAME=$(date +%Y%m%d-%H-%M-%S.jpg)
     curl "http://192.168.15.11/cam-hi.jpg" -o "$FILENAME" --silent
-    gsutil cp $FILENAME "gs://esp32cam-timelapse/$FILENAME"
+    gsutil cp "$FILENAME" "gs://esp32cam-timelapse/$FILENAME"
     sleep 1;
-    rm $FILENAME
+    rm "$FILENAME"
 done
