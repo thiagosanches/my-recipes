137adc92aafc275b4986e2c8917dea8f0ce8b796
Thiago Sanches <tsigwt@gmail.com>
2022-07-30 13:43:31 -0300

    Update block-screen-fun.sh

--- linux/bin/block-screen-fun.sh
+++ linux/bin/block-screen-fun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# It blocks the screen with a screenshot of what was in place.
+# It blocks the screen with a screenshot letting people thinking the computer was not blocked.
+rm -rf /tmp/image-screenshot.png
 scrot -f /tmp/image-screenshot.png
 i3lock -i /tmp/image-screenshot.png
