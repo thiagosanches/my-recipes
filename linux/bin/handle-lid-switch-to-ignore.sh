9db5bb3fcd3f0d9ed993e6552bd9fe50f9f03d29
Thiago Sanches <tsigwt@gmail.com>
2022-08-20 10:12:53 -0300

    Create handle-lid-switch-to-ignore.sh

---
+++ linux/bin/handle-lid-switch-to-ignore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Id does nothing when the lid is closed.
+sudo sed -r 's/#HandleLidSwitch=(.*)/HandleLidSwitch=ignore/g' -i /etc/systemd/logind.conf
+sudo systemctl restart systemd-logind.service
