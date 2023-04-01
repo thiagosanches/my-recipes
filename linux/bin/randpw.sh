516af9b7b204cc9b66fca566059a95bb8bf53eb6
Thiago Sanches <tsigwt@gmail.com>
2023-04-01 08:39:46 -0300

    Update the randpwd - remove some letters

--- linux/bin/randpw.sh
+++ linux/bin/randpw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-function randpw() { < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-20}"; echo; }
+function randpw() { < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-20}" | sed 's/[DdSsEeFfQqCc64]/!/g'; echo; }
 randpw "$@"
