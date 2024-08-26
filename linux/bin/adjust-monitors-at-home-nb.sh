8e5fd6a058037f1ff1aaf4cd7e7f07438e37b0ba
Thiago Sanches <tsigwt@gmail.com>
2024-08-26 08:28:22 -0300

    Update the positions for the monitors

--- linux/bin/adjust-monitors-at-home-nb.sh
+++ linux/bin/adjust-monitors-at-home-nb.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-xrandr --output HDMI-2 --auto --left-of eDP-1
-xrandr --output DP-1 --rotate left
+# notebook's monitor
+xrandr --output eDP-1 --off
+
+# primary monitor
+xrandr --output DP-1 --auto --right-of HDMI-1 --primary
+
+# left
+xrandr --output HDMI-1 --rotate left
+xrandr --output DP-1 --pos 1080x0
