a519541d57dafab59326660876ec0bc60621938f
Thiago Sanches <tsigwt@gmail.com>
2025-05-20 08:11:42 -0300

    Update

--- linux/bin/adjust-monitors-at-home-nb.sh
+++ linux/bin/adjust-monitors-at-home-nb.sh
@@ -4,8 +4,8 @@
 xrandr --output eDP-1 --off
 
 # primary monitor
-xrandr --output DP-1 --auto --right-of HDMI-1 --primary
+xrandr --output HDMI-1 --auto --right-of DP-1 --primary
 
 # left
-xrandr --output HDMI-1 --rotate left
-xrandr --output DP-1 --pos 1080x0
+xrandr --output DP-1 --rotate left
+xrandr --output HDMI-1 --pos 1080x0
