316228e7c08251ee98bddda31003cb447d4becc9
Thiago Sanches <tsigwt@gmail.com>
2025-08-02 09:49:26 -0300

    Update to add simple script that generate timelapse based on jpg filez

---
+++ generate-timelapse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TIMELAPSE=$(date +%Y%m%d%H%M)
+INPUT_FILE="timelapse-${TIMELAPSE}.txt"
+OUTPUT_FILE="timelapse-${TIMELAPSE}.mp4"
+
+# Create just a input file to concat the images on ffmpeg.
+for i in $(ls *.jpg -v); do echo "file '$i'" >> "$INPUT_FILE"; done
+
+# Generate the timelapse video using ffmpeg
+ffmpeg -f concat -input "$INPUT_FILE" -framerate 30 -vcodec libx264 -threads 2 -b:v 10000k "$OUTPUT_FILE"
+rm -rf "$INPUT_FILE"
