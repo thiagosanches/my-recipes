d8dffe42522d04078151b658c734e17ffd1c1c10
Thiago Sanches <tsigwt@gmail.com>
2025-08-16 08:45:38 -0300

    Fix an issue on the ffmpeg

--- linux/bin/generate-timelapse.sh
+++ linux/bin/generate-timelapse.sh
@@ -6,7 +6,5 @@ OUTPUT_FILE="timelapse-${TIMELAPSE}.mp4"
 
 # Create just a input file to concat the images on ffmpeg.
 for i in $(ls *.jpg -v); do echo "file '$i'" >> "$INPUT_FILE"; done
-
-# Generate the timelapse video using ffmpeg
 ffmpeg -f concat -i "$INPUT_FILE" -framerate 30 -vcodec libx264 -threads 2 -b:v 10000k "$OUTPUT_FILE"
 rm -rf "$INPUT_FILE"
